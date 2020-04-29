b96884f776e688dd86645d831d88f3e70e57dc43
RANDRIANASOLOFranco <47238145+RANDRIANASOLOFranco@users.noreply.github.com>
2020-04-29 14:13:37 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+
+
+int main()
+{
+    
+int nbr, i;
+    
+printf("Tapez un nombre entier \n");
+    
+scanf("%d",&nbr);
+    
+for(i = 0; i <= nbr; i++)
+    
+{
+        
+printf("%d \n",i);    
+    
+}
+    
+return 0;
+}
